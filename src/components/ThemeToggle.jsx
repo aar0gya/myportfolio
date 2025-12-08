f8c04bad9259c51293fc763a24abaddbff9a25ae
aar0gya <149552442+aar0gya@users.noreply.github.com>
2025-12-08 15:06:53 +0545

    theme toggle fixed

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -31,7 +31,7 @@ export const ThemeToggle = () => {
     return (
 
         <button onClick={toggleTheme} className={cn(
-            "fixed max-sm:hidden top-4 right-15 z-50 p-2 rounded-full transition-colors duration-300",
+            "fixed max-sm:hidden top-4 right-5 z-50 p-2 rounded-full transition-colors duration-300",
             "focus:outline-hidden"
         )}>
             {""}
